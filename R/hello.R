190cb2e93ec9011aec8f0c2a476c6b1334f38260
RicardoVB2002 <ricardovbaaa6@gmail.com>
2024-07-04 16:09:58 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+
+digitos <- function(Numero_3_digitos) {
+  N<- Numero_3_digitos
+  if(N>=100 & N< 1000){
+    dig1 <- trunc(N/100)
+    x1<- N %% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1 %% 10
+    s <- dig1 + dig2 + dig3
+    print("La suma de los digitos es:")
+    print(s)
+  }else{
+    print("No es numero de 3 digitos")
+  }
+
+}
